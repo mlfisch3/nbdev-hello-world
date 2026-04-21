72d22980678db36a92c270f1e17f05557470ca33
Michael Fischbein <38837568+mlfisch3@users.noreply.github.com>
2026-04-20 23:25:38 -0400

    Fix pkg_resources import for Python 3.12+

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from pkg_resources import parse_version
+#from pkg_resources import parse_version
+
+from packaging.version import Version as parse_version
 from configparser import ConfigParser
 import setuptools, shlex
 assert parse_version(setuptools.__version__)>=parse_version('36.2')
